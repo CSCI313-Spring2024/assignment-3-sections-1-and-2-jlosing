2e0e246f99ca704a4dc79be4a99309274004e15b
jlosing <jwlosing@gmail.com>
2025-03-18 14:12:43 -0500

    Integrated details page into the application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,8 +8,13 @@ export const routes: Routes = [
         component: HomeComponent,
         title: 'Home page',
     },
+    // {
+    //     path: 'details',
+    //     component: DetailsComponent,
+    //     title: 'Home Details'
+    // },
     {
-        path: 'details',
+        path: 'details/:id',
         component: DetailsComponent,
         title: 'Home Details'
     },
